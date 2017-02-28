0303a955d3896653dd615ebb90cb2e6aa8cfdfcf
M. Fauzan Putra <pm.fauzanputra@gmail.com>
2017-02-28 13:48:19 +0700

    Add files via upload

---
+++ Tugas1.java
@@ -0,0 +1,7 @@
+class Mahasiswa {
+	public static void main(String[] args){
+		System.out.println("Nama Lengkap   : M. Fauzan Putra");
+		System.out.println("NIM            : 10114012");
+		System.out.println("Alamat         : Jl. Sekemala no. 15");
+	}
+}
